3cb6e83827af9eb19b94f23a9fb6c352865c50ce
GridexX <arsene582@gmail.com>
2022-07-15 18:28:32 +0200

    chore: use function in main

--- controller/src/main.rs
+++ controller/src/main.rs
@@ -1,52 +1,25 @@
-use actix_web::middleware::Logger;
-use actix_web::{web, App, HttpResponse, HttpServer};
-use kudo_controller_lib::external_api;
-use kudo_controller_lib::internal_api;
-use kudo_controller_lib::{
-    controller::controller_service_client::ControllerServiceClient,
-    controller::controller_service_server::ControllerServiceServer,
-};
-use tonic::transport::Server;
+
+use kudo_controller_lib::internal_etcd;
+use etcd_client::Error;
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    env_logger::init();
+async fn main() -> Result<(), Error> {
+    let mut etcd = internal_etcd::interface::EtcdInterface::new().await;
 
-    let grpc_client_addr = "http://0.0.0.0:50051";
-
-    let grpc_server_addr = "0.0.0.0:50051";
-
-    let http_server_addr = "0.0.0.0:3000";
-
-    //gRPC Client
-    tokio::spawn(async move {
-        ControllerServiceClient::connect(grpc_client_addr.clone())
-            .await
-            .unwrap();
-    });
-
-    //gRPC Server
-    tokio::spawn(async move {
-        Server::builder()
-            .add_service(ControllerServiceServer::new(
-                internal_api::interface::ControllerInterface::default(),
-            ))
-            .serve(grpc_server_addr.clone().parse().unwrap())
-            .await
-            .unwrap();
-    });
-
-    //HTTP Server
-    HttpServer::new(move || {
-        App::new()
-            .route("/health", web::get().to(HttpResponse::Ok))
-            .service(external_api::workload::controller::get_service())
-            .wrap(Logger::default())
-    })
-    .bind(http_server_addr.clone())?
-    .run()
-    .await
-    .unwrap();
+    // PUT
+    etcd.put("hello", "world").await?;
+    let mut resp = etcd.get("hello").await?;
+    println!("{:?}", resp);
+    
+    // PATCH
+    etcd.patch("hello", "kudo").await?;
+    resp = etcd.get("hello").await?;
+    println!("{:?}", resp);
+    
+    // DELETE
+    etcd.delete("hello").await?;
+    resp = etcd.get("hello").await?;
+    println!("{:?}", resp);
 
     Ok(())
 }
