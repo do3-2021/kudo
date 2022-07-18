1f914373197621af24454cccfc2bf310792471f8
esteban <esteban.baron@etu.umontpellier.fr>
2022-07-18 19:24:24 +0200

    wip: Init node agent api implementation
    Signed-off-by: esteban <esteban.baron@etu.umontpellier.fr>

--- node-agent/src/main.rs
+++ node-agent/src/main.rs
@@ -1,3 +1,53 @@
-fn main() {
-    println!("Hello, world!");
+use tonic::{transport::Server, Request, Response, Status};
+
+pub mod node_agent {
+    tonic::include_proto!("node_agent");
+}
+
+use node_agent::instance_service_server::{InstanceService, InstanceServiceServer};
+use node_agent::{Instance, InstanceStatus, SignalInstruction, Status as WorkloadStatus};
+
+#[derive(Debug, Default)]
+pub struct InstanceServiceController {}
+
+#[tonic::async_trait]
+impl InstanceService for InstanceServiceController {
+    type createStream = tonic::Streaming<InstanceStatus>;
+
+    async fn create(
+        &self,
+        _request: Request<Instance>,
+    ) -> Result<Response<Self::createStream>, Status> {
+        // todo
+        println!("create methode");
+
+        Ok(Response::new(Self::createStream::new(
+            "uuid1",
+            WorkloadStatus::Starting,
+            "description",
+        )))
+    }
+
+    async fn signal(&self, _request: Request<SignalInstruction>) -> Result<Response<()>, Status> {
+        // TODO
+        let message = _request.into_inner();
+        println!("{:?}", message);
+
+        Ok(Response::new(()))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "127.0.0.1:56789".parse()?;
+    let instance_service_controller = InstanceServiceController::default();
+
+    println!("Node Agent server listening on {}", addr);
+
+    Server::builder()
+        .add_service(InstanceServiceServer::new(instance_service_controller))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
