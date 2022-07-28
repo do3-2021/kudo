016b0a6142cb8f26ff38571238ae4cac25f4891d
Maxime <maxime.pizzolitto.82@gmail.com>
2022-07-28 20:26:19 +0200

    chore : add test for WorkloadController

--- controller/lib/src/external_api/workload/controller.rs
+++ controller/lib/src/external_api/workload/controller.rs
@@ -78,3 +78,82 @@ pub fn get_services() -> Scope {
         .service(web::resource("/").route(web::put().to(WorkloadController::put_workload))
             .route(web::get().to(WorkloadController::get_all_workloads)))                                         
 }
+#[cfg(test)]
+mod tests {
+    use actix_web::{http::{header::ContentType, StatusCode}, test, web, App ,};
+    use super::WorkloadController;
+    use crate::external_api::workload::model::Workload;
+    use serde_json;
+    use std::str::from_utf8;
+
+    #[actix_web::test]
+    async fn test_put_and_get() {
+        // create temp app http for test
+        let app = test::init_service(
+            App::new().service(web::scope("/workload")
+            .service(web::resource("/")
+                .route(web::put().to(WorkloadController::put_workload)))
+            .service(web::resource("/{workload_id}")
+                .route(web::get().to(WorkloadController::get_workload)))
+    )).await;
+        // create put request to insert workload
+        let payload = r#"{"name":"nginx","environment" : ["NGINX_PROXY=false"],"ports": ["80", "443"]}"#;
+        let req_put = test::TestRequest::put().uri("/workload/")
+        .insert_header(ContentType::json())
+        .set_payload(payload)
+        .to_request();
+        let resp_put = test::call_and_read_body(&app, req_put).await;
+        let workload : Workload = serde_json::from_str(from_utf8(&resp_put).unwrap()).unwrap();   
+        // create get request to get workload width id in url     
+        let path_with_id  = format!("/workload/{}", workload.id);
+        let req_get = test::TestRequest::get().uri(&path_with_id).to_request();
+        let resp_get = test::call_service(&app, req_get).await;
+        assert!(resp_get.status() == StatusCode::OK);
+    }
+
+
+    #[actix_web::test]
+    async fn test_put_and_delete() {
+        // create temp app http for test
+        let app = test::init_service(
+            App::new().service(web::scope("/workload")
+            .service(web::resource("/")
+                .route(web::put().to(WorkloadController::put_workload)))
+            .service(web::resource("/{workload_id}")
+                .route(web::delete().to(WorkloadController::delete_workload)))
+
+    )).await;
+        // create put request to insert workload
+        let payload = r#"{"name":"nginx","environment" : ["NGINX_PROXY=false"],"ports": ["80", "443"]}"#;
+        let req_put = test::TestRequest::put().uri("/workload/")
+        .insert_header(ContentType::json())
+        .set_payload(payload)
+        .to_request();
+        let resp_put = test::call_and_read_body(&app, req_put).await;
+        let workload : Workload = serde_json::from_str(from_utf8(&resp_put).unwrap()).unwrap();   
+        // create get request to get workload width id in url     
+        let path_with_id  = format!("/workload/{}", workload.id);
+        let req_delete = test::TestRequest::delete().uri(&path_with_id).to_request();
+        let resp_delete = test::call_service(&app, req_delete).await;
+        assert!(resp_delete.status() == StatusCode::NO_CONTENT);
+    }
+
+
+
+    #[actix_web::test]
+    async fn test_get_all() {
+        // create temp app http for test
+        let app = test::init_service(
+            App::new().service(web::scope("/workload")
+            .service(web::resource("/")
+                .route(web::get().to(WorkloadController::get_all_workloads)))
+    )).await;  
+        // create get_all request to get all workloads
+        let req_get = test::TestRequest::get().uri("/workload/").to_request();
+        let resp_get = test::call_service(&app, req_get).await;
+        println!("{:?}", resp_get.status());
+        assert!(resp_get.status() == StatusCode::OK);
+    }
+}
+
+
