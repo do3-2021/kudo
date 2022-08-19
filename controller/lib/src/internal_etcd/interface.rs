a6a354a23b09a14286ca3e0e08a5e71dac7d66a7
GridexX <arsene582@gmail.com>
2022-08-19 11:59:33 +0200

    feat: patch and delete method for etcd
    Patch method is used for updating key in etcd
    Delete method is used for deleting key in etcd
    
    Signed-off-by: GridexX <arsene582@gmail.com>

--- controller/lib/src/internal_etcd/interface.rs
+++ controller/lib/src/internal_etcd/interface.rs
@@ -1,4 +1,4 @@
-use etcd_client::{Client, Error, PutResponse  };
+use etcd_client::{Client, Error, PutResponse, DeleteResponse };
 
 pub struct EtcdInterface {
   client: Client,
@@ -23,6 +23,14 @@ impl EtcdInterface {
   pub async fn put(&mut self, key: &str, value: &str) -> Result<PutResponse, Error> {
     self.client.put(key, value, None).await
   }
+
+  pub async fn patch(&mut self, key: &str, value: &str) -> Result<PutResponse, Error> {
+    self.client.put(key, value, None).await
+  }
+
+  pub async fn delete(&mut self, key: &str) -> Result<DeleteResponse, Error> {
+    self.client.delete(key, None).await
+  }
 }
 
 #[cfg(test)]
@@ -34,9 +42,38 @@ mod tests {
   #[tokio::test]
   async fn test_value_insertion() -> Result<(), Error> {
     let mut etcd_client = EtcdInterface::new().await;
-    etcd_client.put("foo", "bar").await?;
+    let _res = etcd_client.put("foo", "bar").await?;
     let resp = etcd_client.get("foo").await?;
     assert_eq!(resp, "bar");
+    Ok(())
+  }
+
+  #[tokio::test]
+  async fn test_value_modification() -> Result<(), Error> {
+    let mut etcd_client = EtcdInterface::new().await;
+    etcd_client.put("foo", "bar").await?;
+    let _res = etcd_client.patch("foo", "baz").await?;
+    let resp = etcd_client.get("foo").await?;
+    assert_eq!(resp, "baz");
+    Ok(())
+  }
+  
+  #[tokio::test]
+  async fn test_value_deletion() -> Result<(), Error> {
+    let mut etcd_client = EtcdInterface::new().await;
+    let _res = etcd_client.put("foo", "bar").await?;
+    let _res = etcd_client.delete("foo").await?;
+    let err = etcd_client.get("foo").await;
+    assert!(err.is_err());
+    Ok(())
+  }
+  
+  #[tokio::test]
+  async fn test_value_deletion_doesnt_exists() -> Result<(), Error> {
+    let mut etcd_client = EtcdInterface::new().await;
+    let _res = etcd_client.put("foo", "bar").await?;
+    let err = etcd_client.delete("foo2").await;
+    assert!(err.is_err());
     Ok(())
   }
 
