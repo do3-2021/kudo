23d12ecbdfcdbe08196859c2e1a5ee0c3d65a139
WoodenMaiden <yann.pomie@laposte.net>
2022-08-26 15:50:18 +0200

    fix: complete signal and add docs
    Signed-off-by: WoodenMaiden <yann.pomie@laposte.net>

--- node-agent/workload_manager/src/workload_manager/mod.rs
+++ node-agent/workload_manager/src/workload_manager/mod.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 use std::sync::mpsc::{Sender, Receiver, channel};
 
 mod workload;
+use tonic::codegen::http::status;
 use workload::Workload;
 
 mod workload_listener;
@@ -12,6 +13,7 @@ use proto::agent::{
     Instance,
     InstanceStatus,
     SignalInstruction,
+    Status as WorkloadStatus
 };
 
 use tonic::Status; 
@@ -34,10 +36,15 @@ impl Default for WorkloadManager {
 
 impl WorkloadManager {
 
+    /// Creates an empty WorkloadManager
     pub fn new() -> Self {
         WorkloadManager::default()
     }
 
+    /// Creates a Workload, run it and starts its listener, a receiver is returned to read all Workloads' status
+    ///
+    /// # Arguments 
+    /// * `instance` - Respresentation of instance to create 
     pub async fn create(&mut self, instance: Instance) -> Result<&Receiver<InstanceStatus>, Status> {
         let (tx, rx) = &self.tx_rx;
         
@@ -57,8 +64,13 @@ impl WorkloadManager {
 //        Err(WorkloadError::new("Error"))
     }
 
+    /// Send a signal to a Workload
+    ///
+    /// # Arguments 
+    /// * `signal_instruction` - Respresentation of signal to send 
     pub async fn signal(&mut self, signal_instruction: SignalInstruction) -> Result<(), Status> {
-        
+        let tx = &self.tx_rx.0;
+
         let workload_id = match signal_instruction.instance {
             Some(inst) => inst.id, 
             None => return Err(
@@ -72,12 +84,39 @@ impl WorkloadManager {
             Some(wrkld) => wrkld
         };
 
+        let status_stopping = InstanceStatus {
+            id: workload_id.clone(),
+            status: WorkloadStatus::Stopping as i32,
+            ..Default::default()
+        };
+        tx.send(status_stopping).unwrap_or(());
+
+
+        let status_destroying = InstanceStatus {
+            id: workload_id.clone(),
+            status: WorkloadStatus::Destroying as i32,
+            ..Default::default()
+        };
+
+        
         match signal_instruction.signal {
-            0 => workload.stop().await, // Status::Stop
-            1 => workload.kill().await, // Status::Kill
+            // Status::Stop
+            0 => { 
+                let promised = workload.stop();
+                tx.send(status_destroying).unwrap_or(());
+                promised.await
+            },
+            // Status::Kill
+            1 => {
+                let promised = workload.kill();
+                tx.send(status_destroying).unwrap_or(());
+                promised.await
+            }
             _ => return Err(Status::not_found("This signal does not exist")) 
         };
 
+
+        self.listeners.remove(&workload_id.clone());
         self.workloads.remove(&workload_id);
 
         Ok(())
